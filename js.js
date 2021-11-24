1f5cfd606f2cc381b9da4ef59386d818831e4eb3
surkhay911 <surkhay77@yandex.ru>
2021-11-24 22:14:50 +0400

    3

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 var width1 = 0;
 var width2 = 0;
+var width3 = 0;
 var widthEnd1 = 50;
-var widthEnd2 = 75;
+var widthEnd2 = 65;
+var widthEnd3 = 80;
 
 
 if (width1<widthEnd1){
@@ -11,7 +13,7 @@ if (width1<widthEnd1){
         document.getElementById("skills-js1").innerHTML =  width1 + '%';
 if(width1>=widthEnd1){
     clearInterval(id1);
-}},25);}
+}},35);}
 
 
 if (width2<widthEnd2){
@@ -21,4 +23,13 @@ if (width2<widthEnd2){
         document.getElementById("skills-css1").innerHTML =  width2 + '%';
 if(width2>=widthEnd2){
     clearInterval(id2);
-}},10);}
+}},30);}
+
+if (width3<widthEnd3){
+    var id3 = setInterval(()=>{
+        width3++; 
+        document.getElementById("skills-html").style.height = width3 + '%'; 
+        document.getElementById("skills-html1").innerHTML =  width3 + '%';
+if(width3>=widthEnd3){
+    clearInterval(id3);
+}},25);}
